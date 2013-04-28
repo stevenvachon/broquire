607bf5f60b48f6147ae8373ecf5017945fd2ba94
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-04-28 16:26:06 -0700

    Fixing tests.

--- test.js
+++ test.js
@@ -3,16 +3,24 @@ if (typeof window === 'undefined') {
   browser = false
 } else {
   browser = window
-  window.broquire = true
+  window.broquire = {test:4}
 }
 
 var assert = require('assert')
   , broquire = require('./')
+  , ok = require('okdone')
+  ;
 
 if (browser) {
   assert.deepEqual(broquire('name'), {})
+  ok('default')
   assert.deepEqual(broquire('name', {test:1}), {test:1})
-  assert.equal(broquire('assert', true), true)
+  ok('value')
+  assert.deepEqual(broquire('name', 'broquire'), {test:4})
+  ok('window')
 } else {
   assert.deepEqual(broquire('assert'), require('assert'))
+  ok('node')
 }
+
+ok.done()
