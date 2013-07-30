29a1b313d23495f143fcc3893981f023df8ada8e
Mikeal Rogers <mikeal.rogers@gmail.com>
2013-07-29 17:11:27 -0700

    Going with standard detection method.

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-var browser
-if (typeof window === 'undefined') {
-  browser = false
-} else {
-  browser = true
-}
-
 module.exports = function (require) {
   function broquire (name, winstr) {
-    if (browser) {
+    if (process.browser) {
       if (typeof winstr === 'undefined') return {}
       if (typeof winstr === 'string') return window[winstr]
       return winstr
